8a49ca9a40617a0ed560e47fcc3e746ad4da2690
Sxlly <shaesullivan.buisness1@gmail.com>
2024-08-24 14:47:22 +0800

    main method

--- elixirscripter/lib/elixirscripter.ex
+++ elixirscripter/lib/elixirscripter.ex
@@ -3,12 +3,25 @@ defmodule Elixirscripter do
   use Application
   def start(_type, _args) do
     #code begin
-    IO.puts(Elixirscripter.hello())
+
+    #call main on run time
+    Elixirscripter.main()
     Supervisor.start_link([], strategy: :one_for_one) #supervisor watches child processes child processes in "[]"
   end
 
+  #dummy hello world method
   def hello do
     :world
   end
+
+  #main method --> called by start method
+  def main do
+    x = 5
+    x = 10
+    IO.puts(x)
+  end
+
+
+
 
 end
