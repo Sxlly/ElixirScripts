9576e291e07db9b72143fd4c14bd08ca1c25e7ee
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-24 20:16:39 +0800

    add fast and slow to list append

--- elixirscripter/lib/EngineStats.ex
+++ elixirscripter/lib/EngineStats.ex
@@ -24,6 +24,17 @@ defmodule Elixirscripter do
     hd(list)
   end
 
+  def add_reading_slow(list, value) do
+    list ++ [value]
+  end
+
+  def add_reading_fast(list, value) do
+    list_reverse = Enum.reverse(list)
+    new_list = [value | list_reverse]
+    Enum.reverse(new_list)
+
+  end
+
 
 
 
