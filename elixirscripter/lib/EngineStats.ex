d64765f140a97034d4b92b268b9ca6582f22067f
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-24 20:28:30 +0800

    removing mod auto run

--- elixirscripter/lib/EngineStats.ex
+++ elixirscripter/lib/EngineStats.ex
@@ -1,13 +1,5 @@
 defmodule Elixirscripter do
 
-  use Application
-  def start(_type, _args) do
-    #code begin
-
-    #call main on run time
-    Elixirscripter.main()
-    Supervisor.start_link([], strategy: :one_for_one) #supervisor watches child processes child processes in "[]"
-  end
 
   def main do
 
--- elixirscripter/mix.exs
+++ elixirscripter/mix.exs
@@ -14,7 +14,6 @@ defmodule Elixirscripter.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {Elixirscripter, []},
       extra_applications: [:logger]
     ]
   end
