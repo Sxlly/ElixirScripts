d6e109f33466359ec5b9af8877c476f5101fcb9b
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-24 16:39:26 +0800

    changes

--- elixirscripter/lib/datetimeapp.ex
+++ elixirscripter/lib/datetimeapp.ex
@@ -1,4 +1,16 @@
-def main do
-  time = Time.new(16, 30, 0, 0)
+defmodule DateTimeApp do
+  alias ElixirLS.LanguageServer.Dialyzer.Supervisor
+  use Application
 
+  def start(_type,_args) do
+    DateTimeApp.main()
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+
+
+  def main do
+    time = Time.new(16, 30, 0, 0)
+
+  end
 end
