6024827a9b2a5f808a0982c1a07a17dd414cbdff
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-24 21:16:23 +0800

    iex shell

--- elixirscripter/lib/EngineStats.ex
+++ elixirscripter/lib/EngineStats.ex
@@ -1,14 +1,19 @@
 defmodule Elixirscripter do
 
+
+  def get_readings() do
+    Enum.map([0,0,0,0,0], fn item -> item = :rand.uniform(100) end)
+  end
+
   def get_latest_reading(list) do
     hd(list)
   end
 
-  def add_reading_slow(list, value) do
+  def add_reading_head(list, value) do
     list ++ [value]
   end
 
-  def add_reading_fast(list, value) do
+  def add_reading_tail(list, value) do
     list_reverse = Enum.reverse(list)
     new_list = [value | list_reverse]
     Enum.reverse(new_list)
